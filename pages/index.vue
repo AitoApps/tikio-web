b2eb1754423b81cd68ceed04fcf6698ea2669263
aitopps <bikchaandroid@gmail.com>
2022-04-23 16:53:11 +0000

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -101,13 +101,13 @@
                 <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                     <div class="sm:text-center lg:text-left">
                         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                            <span class="block xl:inline">Data to enrich your</span>
-                            <span class="block text-indigo-600 xl:inline">online business</span>
+                            <span class="block xl:inline">Tikio App </span>
+                            <span class="block text-indigo-600 xl:inline">Grow Your TikTok Followers Faster</span>
                         </h1>
                         <p
                             class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                            Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                            Use our service to get targeted, high quality, free Tiktok followers and free Tiktok
+                            Likes!</p>
                         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                             <div class="rounded-md shadow">
                                 <a href="#"
